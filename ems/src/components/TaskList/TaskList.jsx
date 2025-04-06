fea7d09e67ec25fa24d29fe9656d16a8c7e0547d
kishangupta99 <kishankumar@gmail.com>
2025-04-07 01:55:54 +0530

    index.css change

---
+++ ems/src/components/TaskList/TaskList.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const TaskList = () => {
+  return (
+    <div id='tasklist' className='h-[50%] overflow-x-auto flex items-center justify-start gap-5 flex-nowrap w-full py-1 mt-16 '>
+       
+        <div className='flex-shrink-0 w-[300px] h-[300px] bg-red-400 rounded-xl flex items-center justify-center mx-2'>
+            <div></div>
+        </div>
+        
+      
+   
+
+      
+    </div>
+  )
+}
+
+export default TaskList
