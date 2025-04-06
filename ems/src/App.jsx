d5ccd41785a3fd66c86f65da37b97b1490ae98c0
kishangupta99 <kishankumar@gmail.com>
2025-04-07 01:49:21 +0530

    second commit

--- ems/src/App.jsx
+++ ems/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Login from './components/Auth/Login'
+import EmpolyeeDashboard from './components/Dashboard/EmpolyeeDashboard'
 
 const App = () => {
   return (
     <>
-      <Login/>
+      {/* <Login/> */}
+      <EmpolyeeDashboard/>
     </>
   )
 }
