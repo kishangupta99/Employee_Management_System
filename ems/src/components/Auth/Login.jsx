c92bf117e35339afc88c4c69b8c8a35d67fb311d
kishangupta99 <kishankumar@gmail.com>
2025-04-07 01:04:24 +0530

    first commit

---
+++ ems/src/components/Auth/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+  <div className='flex h-screen w-screen items-center justify-center'>
+     
+  </div>
+  )
+}
+
+export default Login
