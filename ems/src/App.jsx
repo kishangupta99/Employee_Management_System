bfd2860749e5b69e392e2e2acfcea1698cc49cd0
kishangupta99 <kishankumar@gmail.com>
2025-04-08 00:57:51 +0530

    todays commit

--- ems/src/App.jsx
+++ ems/src/App.jsx
@@ -7,8 +7,8 @@ const App = () => {
   return (
     <>
       {/* <Login/> */}
-      <EmpolyeeDashboard/>
-      {/* <AdminDashboard/> */}
+      {/* <EmpolyeeDashboard/> */}
+      <AdminDashboard/>
     </>
   )
 }
---
+++ ems/src/components/other/AllTask.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const AllTask = () => {
+  return (
+    <div className='bg-[#1c1c1c] p-5 rounded mt-5 h-48 overflow-auto'>
+      <div className='bg-red-400 py-2 mb-2 px-4 flex  justify-between rounded'>
+        <h2>Sarthak</h2>
+        <h3>Make a UI Design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className='bg-red-400 py-2 mb-2 px-4 flex  justify-between rounded'>
+        <h2>Sarthak</h2>
+        <h3>Make a UI Design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className='bg-red-400 py-2 mb-2 px-4 flex  justify-between rounded'>
+        <h2>Sarthak</h2>
+        <h3>Make a UI Design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className='bg-red-400 py-2 mb-2 px-4 flex  justify-between rounded'>
+        <h2>Sarthak</h2>
+        <h3>Make a UI Design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className='bg-red-400 py-2 mb-2 px-4 flex  justify-between rounded'>
+        <h2>Sarthak</h2>
+        <h3>Make a UI Design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className='bg-red-400 py-2 mb-2 px-4 flex  justify-between rounded'>
+        <h2>Sarthak</h2>
+        <h3>Make a UI Design</h3>
+        <h5>Status</h5>
+      </div>
+    </div>
+  )
+}
+
+export default AllTask
